e6ccdf983ab794c8106c5984d0568d4dae957414
Shauna Keating <shauna@truss.works>
2025-11-25 18:13:53 -0500

    starts pmrompts for meaningful sequence

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,9 +1,11 @@
 import * as Rule141 from "./1.4.1.js";
+import * as Rule132 from "./1.3.2.js";
 // ... import others
 
 // map for O(1) lookup
 export const RULES = {
   [Rule141.id]: Rule141,
+  [Rule132.id]: Rule132,
   // the rest of the rules will go here
 };
 
