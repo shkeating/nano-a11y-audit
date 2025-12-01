71a401cd79539d42f6fe40747e835e8b03a2d8d2
Shauna Keating <shauna@truss.works>
2025-11-30 22:56:37 -0500

    creates rule file for 2.2.2 and loads it

--- src/rules/index.js
+++ src/rules/index.js
@@ -2,6 +2,7 @@ import * as Rule141 from "./1.4.1.js";
 import * as Rule132 from "./1.3.2.js";
 import * as Rule133 from "./1.3.3.js";
 import * as Rule253 from "./2.5.3.js";
+import * as Rule222 from "./2.2.2.js";
 
 // ... import others
 
@@ -10,6 +11,7 @@ export const RULES = {
   [Rule141.id]: Rule141,
   [Rule132.id]: Rule132,
   [Rule133.id]: Rule133,
+  [Rule222.id]: Rule222,
   [Rule253.id]: Rule253,
   // the rest of the rules will go here - let's try to keep them in numerical order!
 };
