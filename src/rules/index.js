427c701740f6e697898f9002f6f49eb88f281cc8
Shauna Keating <shauna@truss.works>
2025-12-01 00:55:19 -0500

    adds new rules to rules index

--- src/rules/index.js
+++ src/rules/index.js
@@ -4,19 +4,18 @@ import * as Rule133 from "./1.3.3.js";
 import * as Rule253 from "./2.5.3.js";
 import * as Rule222 from "./2.2.2.js";
 import * as Rule322 from "./3.2.2.js";
+import * as Rule134 from "./1.3.4.js";
+import * as Rule1410 from "./1.4.10.js";
 
-// ... import others
-
-// map for O(1) lookup
 export const RULES = {
   [Rule141.id]: Rule141,
   [Rule132.id]: Rule132,
   [Rule133.id]: Rule133,
+  [Rule134.id]: Rule134,
   [Rule222.id]: Rule222,
   [Rule253.id]: Rule253,
   [Rule322.id]: Rule322,
-  // the rest of the rules will go here - let's try to keep them in numerical order!
+  [Rule1410.id]: Rule1410,
 };
 
-// array for iterating
 export const RULE_LIST = Object.values(RULES);
