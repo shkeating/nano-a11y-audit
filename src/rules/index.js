3c4a6eb3ecd5b054ccf62f9817e17f65b1aa2599
Shauna Keating <shauna@truss.works>
2025-12-04 22:15:03 -0500

    registers new rule file into index

--- src/rules/index.js
+++ src/rules/index.js
@@ -11,6 +11,7 @@ import * as Rule134P from "./1.3.4-portrait.js";
 import * as Rule1410 from "./1.4.10.js";
 import * as Rule1412 from "./1.4.12.js";
 import * as Rule245 from "./2.4.5.js";
+import * as Rule246 from "./2.4.6.js";
 import * as Rule247 from "./2.4.7.js";
 
 export const RULES = {
@@ -24,6 +25,7 @@ export const RULES = {
   [Rule1412.id]: Rule1412,
   [Rule222.id]: Rule222,
   [Rule245.id]: Rule245,
+  [Rule246.id]: Rule246,
   [Rule247.id]: Rule247,
   [Rule253.id]: Rule253,
   [Rule322.id]: Rule322,
